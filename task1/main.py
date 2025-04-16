33f59fec40cde8cecbbbd45b264bff0c3d8d2ff6
Tristan <tristantimp@gmail.com>
2025-04-16 12:41:52 +0200

    bad work

--- task1/main.py
+++ task1/main.py
@@ -1,1 +1,29 @@
 ## runnning script
+import cv2
+import numpy as np
+import os
+
+
+img = cv2.imread('test-image-data\P21-Fg006-R-C01-R01-binarized.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('Image Window', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+'''
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+lines = cv2.HoughLines(edges,1,np.pi/180,200)
+for rho,theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+cv2.imwrite('houghlines3.jpg',img)'''
