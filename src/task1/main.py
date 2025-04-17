860ef33e322b537a13be2efe6082e290f0ef5d2b
giorgosskoul <george.larisa@gmail.com>
2025-04-17 14:48:32 +0200

    Updated get_image functions to also fetch rgb or grayscale when needed

--- src/task1/main.py
+++ src/task1/main.py
@@ -1,7 +1,25 @@
+"""
+Task 01: DSS dataset
+(a) Preprocessing and character segmentation
+(b) Character recognition
+"""
+
+from time import perf_counter
 from preprocessing import *
 
-image_path = "image-data"
 
-dataset = preprocess_image(image_path)
+def main():
 
-hough_transform(dataset)
+    start_time = perf_counter()
+
+    image_path = "image-data"
+
+    dataset = get_binarized_images(image_path)
+
+    hough_transform(dataset)
+
+
+    print(f"Running time for task 01: {round(perf_counter() - start_time),2} seconds")
+
+if __name__ == "__main__":
+    main()
--- src/task1/preprocessing.py
+++ src/task1/preprocessing.py
@@ -2,14 +2,30 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
-def preprocess_image(image_path):
+def get_images(image_path:str, endswith:str):
     dataset = []
     for image in os.listdir(image_path):
-        if image.endswith("binarized.jpg"):
+        if image.endswith(endswith):
             full_path = os.path.join(image_path, image)
             dataset.append(full_path)
-    print(f"Found {len(dataset)} images in the dataset.")
+    print(f"Found {len(dataset)} {endswith} images in the dataset.")
     return dataset
+
+def get_binarized_images(image_path):
+    return get_images(image_path=image_path, endswith='binarized.jpg')
+
+def get_fused_images(image_path):
+    return get_images(image_path=image_path, endswith='fused.jpg')
+
+def get_rgb_images(image_path):
+    dataset = []
+    for image in os.listdir(image_path):
+        if not image.endswith("binarized.jpg") and not image.endswith("fused.jpg"):
+            full_path = os.path.join(image_path, image)
+            dataset.append(full_path)
+    print(f"Found {len(dataset)} rgb images in the dataset.")
+    return dataset 
+
 
 def hough_transform(dataset):
     img = cv2.imread(dataset[0], cv2.IMREAD_GRAYSCALE)
