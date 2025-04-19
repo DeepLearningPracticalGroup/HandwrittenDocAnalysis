83500dce9b16731f69c5a6513a8b972a78283f9e
giorgosskoul <george.larisa@gmail.com>
2025-04-19 16:35:47 +0200

    Generate two scroll lines

--- src/task1/scripts/scroll_generation_baseline.py
+++ src/task1/scripts/scroll_generation_baseline.py
@@ -5,7 +5,7 @@ myenv/bin/ipython src/task1/scripts/scroll_generation_baseline.py
 """
 
 
-from src.task1.utils.generate import generate_synthetic_scroll
+from src.task1.utils.generate import generate_synthetic_multiline_scroll
 import os
 
 # Paths to training data (characters)
@@ -13,7 +13,7 @@ root_folder = 'monkbrill/'
 char_folders = [os.path.join(root_folder, d) for d in os.listdir(root_folder) if os.path.isdir(os.path.join(root_folder, d))]
 
 # Generate scroll lines from characters
-generate_synthetic_scroll(
+generate_synthetic_multiline_scroll(
     output_dir='synthetic_scrolls/',
     char_folders=char_folders,
     canvas_size=(256, 1024),
